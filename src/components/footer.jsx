87bfc083af362867a782a1528a95674d2d0fdb9b
Asikhemhen Osazee <asikhemhenosazee@yahoo.com>
2025-01-28 16:46:54 +0100

    lan update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,18 @@
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
 const Footer = () => {
+  const { t, i18n } = useTranslation(); // Initialize translation hook
+
+  useEffect(() => {
+    i18n.changeLanguage("pt"); // Set default language to Portuguese
+  }, [i18n]); // The empty dependency array ensures this only runs once on mount
+
   return (
     <div className="flex justify-center items-center h-20 bg-white pt-5">
-      <p className="text-sm">Copyright&copy; 2025 All Rights Reserved</p>
+      <p className="text-sm">
+        {t("footer.copyright")} &copy; 2025 {t("footer.allRightsReserved")}
+      </p>
     </div>
   );
 };
