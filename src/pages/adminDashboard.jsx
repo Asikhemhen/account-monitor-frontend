065be6f6b169ba93388f2f2639b3d42783d9360b
Asikhemhen Osazee <asikhemhenosazee@yahoo.com>
2025-01-28 16:28:15 +0100

    language update

--- src/pages/adminDashboard.jsx
+++ src/pages/adminDashboard.jsx
@@ -1,4 +1,5 @@
 import { useAuth } from "../AuthContext";
+import { useEffect } from "react";
 import Card from "../components/card";
 import equity from "../assets/images/equity.svg";
 import accounts from "../assets/images/accounts.svg";
@@ -7,9 +8,11 @@ import withdrawal from "../assets/images/cash.svg";
 import lastWeekProfit from "../assets/images/graph-up.svg";
 import todaysProfit from "../assets/images/graph-flat.svg";
 import totalProfit from "../assets/images/trophy.svg";
+import { useTranslation } from "react-i18next";
 import MainTable from "../components/main-table";
 
 const AdminDashboard = () => {
+  const { t, i18n } = useTranslation();
   const { data, loading, error } = useAuth();
 
   const sumProperty = (array, key) => {
@@ -17,47 +20,51 @@ const AdminDashboard = () => {
     return sum.toFixed(2);
   };
 
+  useEffect(() => {
+    i18n.changeLanguage("pt"); // Set default language to Portuguese
+  }, [i18n]); // The empty dependency array ensures this only runs once on mount
+
   return (
     <section className="flex flex-col pt-4 px-5 mt-28 ml-20 pb-16">
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 pb-10">
         <Card
-          title="Total Accounts"
+          title={t("adminDashboard.totalAccounts")}
           value={data.length}
           img={accounts}
           bg={"bg-orange-500"}
         />
         <Card
-          title="Total Equity"
+          title={t("adminDashboard.totalEquity")}
           value={sumProperty(data, "equity")}
           img={equity}
           bg={"bg-green-300"}
         />
         <Card
-          title="Total Deposit"
+          title={t("adminDashboard.totalDeposit")}
           value={sumProperty(data, "deposit")}
           img={deposit}
           bg={"bg-amber-500"}
         />
         <Card
-          title="Total Withdrawal"
+          title={t("adminDashboard.totalWithdrawal")}
           value={sumProperty(data, "total_withdraw")}
           img={withdrawal}
           bg={"bg-blue-400"}
         />
         <Card
-          title="Last Week Profit"
+          title={t("adminDashboard.lastWeekProfit")}
           value={sumProperty(data, "total_profit_weekly")}
           img={lastWeekProfit}
           bg={"bg-sky-400"}
         />
         <Card
-          title="Today's Profit"
+          title={t("adminDashboard.todaysProfit")}
           value={sumProperty(data, "total_profit_daily")}
           img={todaysProfit}
           bg={"bg-pink-400"}
         />
         <Card
-          title="Total Profit"
+          title={t("adminDashboard.totalProfit")}
           value={sumProperty(data, "total_profit_daily")}
           img={totalProfit}
           bg={"bg-green-600"}
--- src/pages/customerDashboard.jsx
+++ src/pages/customerDashboard.jsx
@@ -6,51 +6,58 @@ import withdrawal from "../assets/images/cash.svg";
 import lastWeekProfit from "../assets/images/graph-up.svg";
 import todaysProfit from "../assets/images/graph-flat.svg";
 import totalProfit from "../assets/images/trophy.svg";
-
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 import MainTable from "../components/main-table";
 
 const CustomerDashboard = () => {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage("pt"); // Set default language to Portuguese
+  }, [i18n]); // The empty dependency array ensures this only runs once on mount
+
   return (
     <section className="flex flex-col pt-4 px-5 mt-28 ml-20 pb-16">
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 pb-10">
         <Card
-          title="Total Accounts"
+          title={t("customerDashboard.totalAccounts")}
           value={6723.36}
           img={accounts}
           bg={"bg-orange-500"}
         />
         <Card
-          title="Total Equity"
+          title={t("customerDashboard.totalEquity")}
           value={6723.36}
           img={equity}
           bg={"bg-green-300"}
         />
         <Card
-          title="Total Deposit"
+          title={t("customerDashboard.totalDeposit")}
           value={6723.36}
           img={deposit}
           bg={"bg-amber-500"}
         />
         <Card
-          title="Total Withdrawal"
+          title={t("customerDashboard.totalWithdrawal")}
           value={6723.36}
           img={withdrawal}
           bg={"bg-blue-400"}
         />
         <Card
-          title="Last Week Profit"
+          title={t("customerDashboard.lastWeekProfit")}
           value={6723.36}
           img={lastWeekProfit}
           bg={"bg-sky-400"}
         />
         <Card
-          title="Today's Profit"
+          title={t("customerDashboard.todaysProfit")}
           value={6723.36}
           img={todaysProfit}
           bg={"bg-pink-400"}
         />
         <Card
-          title="Total Profit"
+          title={t("customerDashboard.totalProfit")}
           value={6723.36}
           img={totalProfit}
           bg={"bg-green-600"}
